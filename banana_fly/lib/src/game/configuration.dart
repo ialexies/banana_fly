371d4c5d7f9808be3c4cfa7e63d2390b0a9aa891
Alexies Iglesia <ialexies@gmail.com>
2024-02-07 20:04:30 +0800

    update configurations

--- banana_fly/lib/src/game/configuration.dart
+++ banana_fly/lib/src/game/configuration.dart
@@ -1,10 +1,18 @@
 // ignore_for_file: public_member_api_docs
 
+// class Config {
+//   static const groundHeight = 110.0;
+//   static const gameSpeed = 200.0;
+//   static const pipeInterval = 1.5;
+//   static const birdVelocity = 210;
+//   static const gravity = -80.0;
+//   static const cloudsHeight = 70.0;
+// }
 class Config {
   static const groundHeight = 110.0;
   static const gameSpeed = 200.0;
   static const pipeInterval = 1.5;
   static const birdVelocity = 210;
-  static const gravity = -80.0;
+  static const gravity = -100.0;
   static const cloudsHeight = 70.0;
 }
